67c4e7e5e24d265f50c1f58078fb525b2bba6dfc
zombieliu <17600047539@163.com>
2022-05-02 01:07:26 +0800

    5-2

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -172,9 +172,104 @@ const Main = () =>{
             </div>
         )
     }else{
-        return <div>
-            1
-        </div>
+        return (
+            <div className="relative">
+                <div className="absolute inset-x-0 bottom-0    " />
+                <div className=" mx-auto  ">
+                    <div className="absolute inset-0">
+                        <img
+                            className="h-screen w-full mx-auto"
+                            src="https://cdn.discordapp.com/attachments/876498266550853642/969529054967529522/5.png"
+                            alt="People working on laptops"
+                        />
+                    </div>
+                    <Header/>
+                    <div className="max-w-7xl relative px-8 pt-20 pb-7 max-h-screen    mx-auto rounded-b-3xl ">
+                        <div>
+                            <Transition
+                                show={true}
+                                enter="transition-opacity ease-linear duration-300"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="transition-opacity ease-linear duration-300"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <div className="border-2 border-gray-400 bg-white rounded-2xl border-b-4 border-r-4">
+                                    <div className="text-center mt-4 mb-10">
+                                        #{pet}
+                                    </div>
+                                    <div className="flex px-4 items-center text-2xl h-56 text-gray-400">
+                                        <div className="">
+                                            <button onClick={left}>
+                                                <i className="fa fa-arrow-left" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                        <Link href="/bag/detail">
+                                            <img className="w-36 mx-auto" src="" alt=""/>
+                                        </Link>
+                                        <div>
+                                            <button onClick={right}>
+                                                <i className="fa fa-arrow-right" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    <div className="flex justify-between px-4 mt-16 mb-3">
+                                        <div className="rounded-full border border-gray-400 w-20 h-5">
+                                        </div>
+                                        <div>
+                                            LV{pet}
+                                        </div>
+                                        <div className="rounded-full border border-gray-400 w-20 h-5">
+                                        </div>
+                                    </div>
+                                </div>
+                            </Transition>
+                        </div>
+
+                        <div className="mt-5 flex justify-between">
+                            <div className="border-2 w-16  rounded-lg  border-gray-400">
+
+                            </div>
+                            <div className="border-2 w-16 h-16 rounded-lg  border-gray-400">
+
+                            </div>
+                            <div className="border-2 w-16 h-16 rounded-lg  border-gray-400">
+
+                            </div>
+                            <div className="border-2 w-16 h-16 rounded-lg  border-gray-400">
+
+                            </div>
+                        </div>
+                        <div className="flex justify-between mt-10 text-3xl text-blue-500 ">
+                            <button>
+                                <i className="fa fa-gift" aria-hidden="true"></i>
+                            </button>
+                            <button>
+                                <i className="fa fa-question-circle-o" aria-hidden="true"></i>
+                            </button>
+                        </div>
+                        <div className="flex justify-center mt-10 items-center ">
+                            <div className="">
+                                <img className="w-32" src="https://cdn.discordapp.com/attachments/876498266550853642/969501553935405076/LEARN.png" alt=""/>
+                            </div>
+                            <div>
+                                <Link href="/answer">
+                                    <div className="">
+                                        <img className="w-32" src="https://cdn.discordapp.com/attachments/876498266550853642/969501554119933962/REVIEW.png" alt=""/>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="text-xs mt-2 flex justify-end px-20">
+                            当前剩余答题次数 2
+                        </div>
+                    </div>
+                    <Navigation/>
+                </div>
+            </div>
+        )
     }
 }
 
